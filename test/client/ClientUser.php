0690c176b710d4d3c71862ca1a592a1bae5af037
Maykonn Welington Candido <maykonn@outlook.com>
2013-10-01 15:10:43 -0300

    Mudança estrutural

---
+++ test/client/ClientUser.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * ClientUser
+ *
+ * This represent the "User" class in client application domain
+ *
+ * @author Maykonn Welington Candido<maykonn@outlook.com>
+ */
+class ClientUser
+{
+    public function __construct()
+    {
+        
+    }
+}
+
+?>
---
+++ test/index.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Teste de RapidAuthorization
+ * @author Maykonn Welington Candido<maykonn@outlook.com>
+ *
+ * Caso sua aplicação possua seu próprio autoload você deve ignorar essa parte.
+ * Caso contrário
+ */
+$sep = DIRECTORY_SEPARATOR;
+require_once '..' . $sep . 'index.php';
+require_once 'client' . $sep . 'ClientUser.php';
+
+use Rapid\Authorization\RapidAuthorization;
+
+$authorizationConf = Array(
+    'useRapidAuthorizationAutoload' => true,
+    'userClassInstance' => new ClientUser(),
+    'mysqlHost' => 'localhost',
+    'mysqlUser' => '',
+    'mysqlPass' => '',
+    'dbName' => 'rapid_authorization',
+    'dbCharset' => 'utf8'
+);
+
+$authorization = new RapidAuthorization($authorizationConf);
+?>
