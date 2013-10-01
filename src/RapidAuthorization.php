77550f607651ba720c35f57db3b2b54700a4394c
Maykonn Welington Candido <maykonn@outlook.com>
2013-10-01 14:44:02 -0300

    Adicionada conexão com banco de dados

--- src/RapidAuthorization.php
+++ src/RapidAuthorization.php
@@ -26,17 +26,39 @@ class RapidAuthorization
     private $preferences;
 
     /**
-     * Get client preferences handler
-     * @return ArrayObject
+     * @var MySQL
      */
-    /* public function getPreferences()
-      {
-      return $this->preferences->getPreferencesList();
-      } */
+    private $mysql;
 
     public function __construct($preferences = Array())
     {
+        $this->initPreferences($preferences);
+        $this->initMySqlHandler();
+        $this->initUserHandler();
+    }
+
+    private function initPreferences(Array $preferences)
+    {
         $this->preferences = ClientPreferences::instance($preferences);
+    }
+
+    private function initMySQL()
+    {
+        $this->mysql = MySQL::instance();
+        $this->mysql->connect(Array(
+            'host' => $this->preferences->mysqlHost,
+            'user' => $this->preferences->mysqlUser,
+            'pass' => $this->preferences->mysqlPass
+        ));
+
+        $this->mysql->selectDb(Array(
+            'dbCharset' => $this->preferences->dbCharset,
+            'dbName' => $this->preferences->dbName
+        ));
+    }
+
+    private function initUserHandler()
+    {
         $this->user = User::instance($this->preferences->userClassInstance);
     }
 
