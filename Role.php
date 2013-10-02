cd3c04d8f254e1393299571e028b81a0c58e2825
Maykonn Welington Candido <maykonn@outlook.com>
2013-10-02 12:00:08 -0300

    Finalizado CRUD de role

--- Role.php
+++ Role.php
@@ -32,13 +32,7 @@ class Role extends Entity
     public function save()
     {
         try {
-            $sql = "
-            INSERT INTO role(
-                id, name
-            ) VALUES (
-                :id, :name
-            ) ON DUPLICATE KEY UPDATE
-                name = :name";
+            $sql = "INSERT INTO role(id, name) VALUES (:id, :name) ON DUPLICATE KEY UPDATE name = :name";
             $stmt = $this->db->prepare($sql);
             $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
             $stmt->bindParam(':name', $this->name, PDO::PARAM_STR);
@@ -50,6 +44,18 @@ class Role extends Entity
 
             $this->id = (int) $this->id;
             return $this->id;
+        } catch(PDOException $e) {
+            MySQL::showException($e);
+        }
+    }
+
+    public function delete()
+    {
+        try {
+            $sql = "DELETE FROM role WHERE id = :id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
+            return $stmt->execute();
         } catch(PDOException $e) {
             MySQL::showException($e);
         }
