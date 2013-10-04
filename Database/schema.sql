027ebae911b408dc32ae5867faaf4a7042cf715a
Maykonn Welington Candido <maykonn@outlook.com>
2013-10-04 18:30:36 -0300

    Atualizado o schema.sql

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,6 +1,16 @@
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
+-- Table structure for operation
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `operation` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for role
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `role` (
@@ -11,11 +21,50 @@ CREATE TABLE IF NOT EXISTS `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
+-- Table structure for role_has_task
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `role_has_task` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_role` int(10) unsigned NOT NULL,
+  `id_task` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_user_2` (`id_role`,`id_task`),
+  KEY `id_role` (`id_role`) USING BTREE,
+  KEY `id_task` (`id_task`) USING BTREE,
+  CONSTRAINT `role_has_task_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `role_has_task_ibfk_2` FOREIGN KEY (`id_task`) REFERENCES `task` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for task
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `task` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for task_has_operation
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `task_has_operation` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_task` int(10) unsigned NOT NULL,
+  `id_operation` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_user_2` (`id_operation`,`id_task`),
+  KEY `id_task` (`id_task`) USING BTREE,
+  KEY `id_operation` (`id_operation`) USING BTREE,
+  CONSTRAINT `task_has_operation_ibfk_1` FOREIGN KEY (`id_task`) REFERENCES `task` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `task_has_operation_ibfk_2` FOREIGN KEY (`id_operation`) REFERENCES `operation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
 -- Table structure for user
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -30,6 +79,6 @@ CREATE TABLE IF NOT EXISTS `user_has_role` (
   UNIQUE KEY `id_user_2` (`id_user`,`id_role`),
   KEY `id_user` (`id_user`),
   KEY `id_role` (`id_role`),
-  CONSTRAINT `user_has_role_ibfk_2` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `user_has_role_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `user_has_role_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_has_role_ibfk_2` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
