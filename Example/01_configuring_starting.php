733acf1bda0f07c0a54a55665ec35a83108baf59
Maykonn Welington Candido <maykonn@outlook.com>
2013-10-07 18:22:23 -0300

    Adicionado exemplo Configuring and starting

--- Example/01_configuring_starting.php
+++ Example/01_configuring_starting.php
@@ -1,11 +1,13 @@
 <?php
 
 /**
- * Configuring and starting RapidAuthorization
+ * Configuring and starting
  * @author Maykonn Welington Candido<maykonn@outlook.com>
  */
 $sep = DIRECTORY_SEPARATOR;
 require_once '..' . $sep . 'RapidAuthorization.php';
+
+use Rapid\Authorization\RapidAuthorization;
 
 // All available params
 $configuration = Array(
@@ -14,17 +16,12 @@ $configuration = Array(
     'mysqlUser' => 'root',
     'mysqlPass' => '',
     'dbName' => 'rapid_authorization',
-    'dbCharset' => 'latin1', // optional param, default is utf8
-    'autoGenerateTables' => false, // optional param, default is true
+    'dbCharset' => 'utf8', // optional param, default is utf8
+    'autoGenerateTables' => true, // optional param, default is true
     'userTable' => 'user_table', // optional param, default is user
     'userTablePK' => 'user_pk', // optional param, default is id
     'useRapidAuthorizationAutoload' => true // optional param, default is false
 );
 
-// Instantiation
-$authorization = new Rapid\Authorization\RapidAuthorization($configuration);
-
-// or instatiate with use
-//use Rapid\Authorization\RapidAuthorization;
-//$authorization = new RapidAuthorization($configuration);
+$authorization = new RapidAuthorization($configuration);
 ?>
