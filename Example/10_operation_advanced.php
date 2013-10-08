7ba7701c32a649d3bd43a6b7f5e267282ea06076
Maykonn Welington Candido <maykonn@outlook.com>
2013-10-08 14:22:32 -0300

    Adicionado exemplo Working with Operations - Advanced

---
+++ Example/10_operation_advanced.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Working with Operations - Advanced
+ * @author Maykonn Welington Candido<maykonn@outlook.com>
+ */
+$sep = DIRECTORY_SEPARATOR;
+require_once '..' . $sep . 'RapidAuthorization.php';
+
+use Rapid\Authorization\RapidAuthorization;
+
+$configuration = Array(
+    'mysqlHost' => 'localhost',
+    'mysqlPort' => 3306,
+    'mysqlUser' => 'root',
+    'mysqlPass' => '',
+    'dbName' => 'rapid_authorization',
+    'userTable' => 'user_table',
+    'userTablePK' => 'user_pk',
+    'useRapidAuthorizationAutoload' => true,
+);
+
+$authorization = new RapidAuthorization($configuration);
+
+// List all Tasks that can execute an Operation
+$operationId = 1;
+echo 'ALL TASKs THAT CAN EXECUTE OPERATION #' .$operationId . '<pre>';
+$tasksThaCanExecute = $authorization->operation()->getTasksThatCanExecute($operationId);
+print_r($tasksThaCanExecute);
+echo '</pre>';
+
+$operationId = 2; // in default examples not exist
+echo 'ALL TASKs THAT CAN EXECUTE OPERATION #' .$operationId . '<pre>';
+$tasksThaCanExecute = $authorization->operation()->getTasksThatCanExecute($operationId);
+print_r($tasksThaCanExecute);
+echo '</pre>';
+
+$operationId = 3;
+echo 'ALL TASKs THAT CAN EXECUTE OPERATION #' .$operationId . '<pre>';
+$tasksThaCanExecute = $authorization->operation()->getTasksThatCanExecute($operationId);
+print_r($tasksThaCanExecute);
+echo '</pre>';
+
+$operationId = 4;
+echo 'ALL TASKs THAT CAN EXECUTE OPERATION #' .$operationId . '<pre>';
+$tasksThaCanExecute = $authorization->operation()->getTasksThatCanExecute($operationId);
+print_r($tasksThaCanExecute);
+echo '</pre>';
