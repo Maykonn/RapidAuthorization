f94c92613ea0c330810216d15118ace486b1b503
Maykonn Welington Candido <maykonn@outlook.com>
2013-10-23 12:40:22 -0200

    Adicionado business_name e modificado name para opcional.

--- Role.php
+++ Role.php
@@ -206,7 +206,7 @@ class Role extends Entity
 
             $stmt = $this->db->prepare($sql);
             $stmt->bindParam(':id', $this->id, PDO::PARAM_INT);
-            $stmt->bindParam(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindParam(':businessName', $this->business_name, PDO::PARAM_STR);
 
             $name = ($this->name ? $this->name : null);
             $stmt->bindParam(':name', $name);
